a4f44980ad98657e86fa8976b4afe49886873f70
DeLucca990 <pedruca2001@gmail.com>
2023-09-05 10:51:54 -0300

    handout 1 feito

---
+++ notes/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .models import Note
+
+def index(request):
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        note = Note.objects.create(title=title, content=content)
+        note.save()
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        notes_data = [{'title': note.title, 'content': note.content} for note in all_notes]
+        print(notes_data)
+        return render(request, 'notes/index.html', {'notes': notes_data})
